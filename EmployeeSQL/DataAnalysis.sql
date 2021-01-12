cb30d7e887b6b880aca4762887948e3c220e1da9
Prarthna <prarthna76@gmail.com>
2021-01-11 23:11:49 -0500

    Step 7 of Data Analysis

--- EmployeeSQL/DataAnalysis.sql
+++ EmployeeSQL/DataAnalysis.sql
@@ -59,3 +59,14 @@ INNER JOIN employees AS e
 ON de.emp_no = e.emp_no
 WHERE dept_name = 'Sales'
 ORDER BY de.emp_no;
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales' 
+OR departments.dept_name = 'Development';
+
